4bde23f30ff45a5bbb1e10e45d19650b950a84e1
avc <dawid_galus@wp.pl>
2020-05-06 22:31:16 +0200

    ip address

---
+++ src/ip_address.cpp
@@ -0,0 +1,77 @@
+#include "ip_address.hpp"
+
+IpAddress::IpAddress()
+{
+    this->octets[0] = 0;
+    this->octets[1] = 0;
+    this->octets[2] = 0;
+    this->octets[3] = 0;
+}
+
+IpAddress::IpAddress(uint8_t oct1, uint8_t oct2, uint8_t oct3, uint8_t oct4)
+{
+    this->octets[0] = oct1;
+    this->octets[1] = oct2;
+    this->octets[2] = oct3;
+    this->octets[3] = oct4;
+}
+
+IpAddress::IpAddress(uint32_t ipAddress)
+{
+    this->octets[0] = ipAddress >> 24;
+    this->octets[1] = (ipAddress >> 16) & 0xFF;
+    this->octets[2] = (ipAddress >> 8) & 0xFF;
+    this->octets[2] = ipAddress & 0xFF;
+}
+
+IpAddress::IpAddress(const IpAddress& ipAddress)
+{
+    uint32_t ip = ipAddress.to_bytes_be();
+    this->octets[0] = ip >> 24;
+    this->octets[1] = (ip >> 16) & 0xFF;
+    this->octets[2] = (ip >> 8) & 0xFF;
+    this->octets[2] = ip & 0xFF;
+}
+
+std::string IpAddress::to_string() const
+{
+    return std::to_string(this->octets[0]) + "." + std::to_string(this->octets[1]) + "." + std::to_string(this->octets[2]) + "." + std::to_string(this->octets[3]);
+}
+
+std::string IpAddress::to_hex_string_be() const
+{
+    std::stringstream ss;
+    ss << std::hex << this->octets[0] << std::hex << this->octets[1] << std::hex << this->octets[2] << std::hex << this->octets[3];
+    return ss.str();
+}
+
+std::string IpAddress::to_hex_string_le() const
+{
+    std::stringstream ss;
+    ss << std::hex << this->octets[3] << std::hex << this->octets[2] << std::hex << this->octets[1] << std::hex << this->octets[0];
+    return ss.str();
+}
+
+uint32_t IpAddress::to_bytes_be() const
+{
+    uint32_t ret = this->octets[0];
+    ret <<= 8;
+    ret += this->octets[1];
+    ret <<= 8;
+    ret += this->octets[2];
+    ret <<= 8;
+    ret += this->octets[3];
+    return ret;
+}
+
+uint32_t IpAddress::to_bytes_le() const
+{
+    uint32_t ret = this->octets[3];
+    ret <<= 8;
+    ret += this->octets[2];
+    ret <<= 8;
+    ret += this->octets[1];
+    ret <<= 8;
+    ret += this->octets[0];
+    return ret;
+}
---
+++ src/structures.hpp
@@ -0,0 +1,68 @@
+#ifndef STRUCTURES_HPP_
+#define STRUCTURES_HPP_
+
+#include <string>
+#include <vector>
+
+#include <cstdint>
+
+
+#define UNKNOWN 0
+#define FROM_SRC_TO_DST 1
+#define FROM_DST_TO_SRC 2
+
+
+
+struct TCPSession {
+    std::string ip_src;
+    uint16_t src_port;
+    std::string ip_dst;
+    uint16_t dst_port;
+    bool is_active;
+    std::string first_segm_tstmp;
+    std::string last_segm_tstmp;
+    std::string remote_geolocation;
+};
+
+struct TCPSessionMin {
+    std::string ip_src;
+    uint16_t src_port;
+    std::string ip_dst;
+    uint16_t dst_port;
+};
+
+struct TCPSegment {
+    std::vector<std::string> flags;
+    int size;
+    std::string timestamp;
+    int direction;
+};
+
+struct ICMPSegment {
+    std::string ip_src;
+    std::string ip_dst;
+    uint8_t type;
+    std::string timestamp;
+};
+
+struct UDPSegment {
+    std::string ip_src;
+    uint16_t src_port;
+    std::string ip_dst;
+    uint16_t dst_port;
+    int size;
+    std::string timestamp;
+};
+
+struct ARPPacket {
+
+};
+
+struct HTTPSite {
+    std::string ip;
+    std::string domain;
+    std::string url;
+    bool update = false;
+};
+
+#endif
