6f030ff9447ed15668220840c05c79a11602b1db
avc <dawid_galus@wp.pl>
2020-05-07 15:24:11 +0200

    utils, getconfig

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,21 @@
 #include "../libs/rapidjson/rapidjson.h"
 #include "../libs/rapidjson/document.h"
 
-#include <chrono>
-#include <iomanip>
-#include <ctime>
+void getConfig(char* path, rapidjson::Document& config)
+{
+    std::ifstream c(path);
+    std::stringstream buffer;
+    buffer << c.rdbuf();
+    std::string _c = buffer.str();
+    config.Parse(_c.c_str());
+}
 
 
 int main(int argc, char* argv[])
 {
-    std::ifstream c(argv[1]);
-    std::stringstream buffer;
-    buffer << c.rdbuf();
-    std::string _c = buffer.str();
-
     rapidjson::Document config;
-    config.Parse(_c.c_str());
+    getConfig(argv[1], config);
 
-    std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
-    std::time_t in_time_t = std::chrono::system_clock::to_time_t(tp);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
-    std::cout << ss.str() << std::endl;
-    
     std::cout << config["interface"].GetString() << std::endl;
 
     return 0;
---
+++ src/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+static std::string getCurrentDatetime(std::string format)
+{
+    std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+    std::time_t in_time_t = std::chrono::system_clock::to_time_t(tp);
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), format.c_str());
+    return ss.str();
+}
+
+#endif
