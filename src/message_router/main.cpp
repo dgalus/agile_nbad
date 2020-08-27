499bdc6a0f9a5643286300f9c6094a02a25a0ecf
avc <dawid_galus@wp.pl>
2020-08-28 00:14:04 +0200

    file socket

--- src/message_router/main.cpp
+++ src/message_router/main.cpp
@@ -10,12 +10,28 @@
 #include <csignal>
 #include <fstream>
 
+#include <fcntl.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 void signal_handler(int signal)
 {
     // stop loop, cleanup everything
 }
 
-
+void file_socket(std::string filename)
+{
+    struct sockaddr_un namesock;
+    int fd;
+    namesock.sun_family = AF_UNIX;
+    strncpy(namesock.sun_path, (char *) filename.c_str(), sizeof(namesock.sun_path));
+    fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    bind(fd, (struct sockaddr *) &namesock, sizeof(struct sockaddr_un));
+    close(fd);
+}
 
 int main()
 {
